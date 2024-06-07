c0f835294eda3e333e332e247d2dc29356b98ef6
sendos105 <sendosalmansour@gmail.com>
2024-06-07 11:44:51 -0400

    last ar

--- assets/js/p.js
+++ assets/js/p.js
@@ -22,3 +22,95 @@ const slideImage=()=>{
         }
     )
 }
+
+
+//////////////////////////////////////////////////
+
+let arabicp1 =document.getElementById('arabicP1')
+let englishp1 =document.getElementById('EnglishP1')
+
+let titleP1=document.getElementById('titleP1')
+let loc=document.getElementById('loc')
+let fur=document.getElementById('fur')
+let tow=document.getElementById('tow')
+let ro=document.getElementById('ro')
+let bath=document.getElementById('bath')
+let si=document.getElementById('si')
+let mo=document.getElementById('mo')
+let ye=document.getElementById('ye')
+let ser=document.getElementById('ser')
+let fea=document.getElementById('fea')
+
+//values/////
+
+let locV=document.getElementById('locV')
+let furV=document.getElementById('furV')
+let towV=document.getElementById('towV')
+let roV=document.getElementById('roV')
+let bathV=document.getElementById('bathV')
+let siV=document.getElementById('siV')
+let moV=document.getElementById('moV')
+let yeV=document.getElementById('yeV')
+let serV=document.getElementById('serV')
+let feaV=document.getElementById('feaV')
+
+
+
+arabicp1.onclick=()=>{
+  setLangaugep1("arabicP1")
+}
+englishp1.onclick=()=>{
+  setLangaugep1('englishp1')
+}
+function setLangaugep1(getlangauge){
+  if(getlangauge==="arabicP1"){
+    titleP1.innerHTML=" شقة للإيجار"
+    loc.innerHTML="الموقع"
+    fur.innerHTML="الأثاث"
+    tow.innerHTML="البرج"
+    ro.innerHTML="عدد الغرف"
+    bath.innerHTML="عدد الحمامات"
+    si.innerHTML="المساحة"
+    mo.innerHTML="الإيجار الشهري"
+    ye.innerHTML="الإيجار السنوي"
+    ser.innerHTML="الخدمات"
+    fea.innerHTML="الميزات"
+    ////////////////////VALUES
+    locV.innerHTML="وسط المدينة"
+    furV.innerHTML="مفروشة بالكامل"
+    towV.innerHTML="ابر كريست "
+    roV.innerHTML="2BR ماستر "
+    bathV.innerHTML=" ٣"
+    siV.innerHTML="1219.12 قدم مربع"
+    moV.innerHTML="18.000 AED "
+    yeV.innerHTML=" 180.000 AED"
+    serV.innerHTML="جيم + مسبح + سوبر ماركت + صيدلية "
+    feaV.innerHTML="خدمة صف السيارات + فرش فندقي كامل + اطلالة برج خليفة + قرب دبي مول + صالة العاب للاطفال"
+  }
+else if(getlangauge==="englishp1"){
+  titleP1.innerHTML="apartment for Rent"
+  loc.innerHTML="Location:"
+  fur.innerHTML="Furniture:"
+  tow.innerHTML="Tower:"
+  ro.innerHTML="Rooms:"
+  bath.innerHTML="Bathroom:"
+  si.innerHTML="Size:"
+  mo.innerHTML="Rent Per Month:"
+  ye.innerHTML="Rent Per Year:"
+  ser.innerHTML="Services:"
+  fea.innerHTML="Features:"
+  //////////////////////VALUES
+  locV.innerHTML="Downtown"
+  furV.innerHTML="full"
+  towV.innerHTML="Upper Crest"
+  roV.innerHTML="2BR master "
+  bathV.innerHTML="3 "
+  siV.innerHTML="1219.12 ft²"
+  moV.innerHTML="18.000 AED"
+  yeV.innerHTML=" 180.000 AED"
+  serV.innerHTML="GYM, swimming pool, Supermarket, drugstore"
+  feaV.innerHTML="parking, Full hotel furniture, Burj Khalifa view, Near Dubai Mall, Children's play hall"
+
+}
+
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -78,24 +78,24 @@ window.addEventListener("scroll", function () {
 
 
 /////////////////////////////////////////////////////////
-const observe = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=>{
-      if(entry.isIntersecting){
-          entry.target.classList.add('show-items')
-      }
-      else{
-          entry.target.classList.remove('show-items')
-      }
+const observe = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-items')
+    }
+    else {
+      entry.target.classList.remove('show-items')
+    }
   })
 })
 
-const scrollscale=document.querySelectorAll('.scroll-scale')
-scrollscale.forEach((el)=>observe.observe(el))
+const scrollscale = document.querySelectorAll('.scroll-scale')
+scrollscale.forEach((el) => observe.observe(el))
 
 
 
-const scrolltop=document.querySelectorAll('.scroll-top')
-scrolltop.forEach((el)=>observe.observe(el))
+const scrolltop = document.querySelectorAll('.scroll-top')
+scrolltop.forEach((el) => observe.observe(el))
 
 
 
@@ -106,14 +106,158 @@ scrolltop.forEach((el)=>observe.observe(el))
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-function rotate(){
-  var lastChild=$('.sliderP div:last-child').clone();
+function rotate() {
+  var lastChild = $('.sliderP div:last-child').clone();
   $('.sliderP div').removeClass('firstSlide')
   $('.sliderP div:last-child').remove();
   $('.sliderP').prepend(lastChild)
   $(lastChild).addClass('firstSlide')
 }
 
-window.setInterval(function(){
+window.setInterval(function () {
   rotate()
-},4000);
+}, 4000);
+
+
+
+
+/////////////////////////language////////////////////////////////
+
+let arabic = document.getElementById('arabic')
+let english = document.getElementById('English')
+let topFont = document.querySelectorAll('.topFont')
+let preloadH2 = document.getElementById('preloadH2')
+let title = document.getElementById('title')
+let navitem = document.querySelectorAll('.navbar1-item')
+let home = document.getElementById('home')
+let needusDiv = document.getElementById('needusDiv')
+let need = document.getElementById('need')
+let value1 = document.getElementById('val1A')
+let value2 = document.getElementById('val2A')
+let value3 = document.getElementById('val3A')
+let About = document.getElementById('About')
+let Latest = document.getElementById('Latest')
+let Partners = document.getElementById('Partners')
+let Contact = document.getElementById('Contact')
+let Visit = document.getElementById('Visit')
+let locat = document.getElementById('locat')
+let Available = document.getElementById('Available')
+let Relax = document.getElementById('Relax')
+let phone = document.getElementById('phone')
+let aboutus = document.getElementById('aboutus')
+let aboutText = document.getElementById('aboutText')
+let forrent = document.getElementById('forrent')
+let forSale = document.getElementById('forSale')
+let ContactUs = document.getElementById('ContactUs')
+let ContactText = document.getElementById('ContactText')
+
+
+arabic.onclick = () => {
+  setLangauge("arabic")
+}
+english.onclick = () => {
+  setLangauge('english')
+}
+
+
+
+function setLangauge(getlangauge) {
+  if (getlangauge === "arabic") {
+
+    for (let i = 0; i < topFont.length; i++) {
+      topFont[i].style.fontFamily = "Marhey, sans-serif"
+      
+    }
+    title.innerHTML = "جد المكان الذي يلامس روحك"
+    home.innerHTML = "الصفحة الرئيسية"
+    for (let i = 0; i < navitem.length; i++) {
+      navitem[i].style.display = "flex";
+      navitem[i].style.justifyContent = "end";
+    }
+    needusDiv.innerHTML = ` 
+<div class="radio-input">
+<input value="value-1" name="value-radio" id="value-1" type="radio">
+  <label for="value-1"><a id="val1A"
+      href="https://api.whatsapp.com/send/?phone=%2B971507004348&text&type=phone_number&app_absent=0">للأجار</a></label>
+  <input value="value-2" name="value-radio" id="value-2" type="radio">
+  <label for="value-1"><a id="val2A"
+      href="https://api.whatsapp.com/send/?phone=%2B971507004348&text&type=phone_number&app_absent=0">للبيع</a></label>
+  <input value="value-3" name="value-radio" id="value-3" type="radio">
+  <label for="value-1"><a id="val3A"
+      href="https://api.whatsapp.com/send/?phone=%2B971507004348&text&type=phone_number&app_absent=0"> العقارات</a></label>
+  <p style="margin: auto; margin-left: 0;" id="need"> :أنت بحاجتنا من أجل</p>
+  
+</div>
+`
+
+    need.innerHTML = 'أنت بحاجتنا من أجل :'
+    value1.innerHTML = 'للأجار '
+    value2.innerHTML = 'للبيع'
+    value3.innerHTML = ' العقارات'
+    About.innerHTML = 'من نحن؟'
+    Latest.innerHTML = 'أخر العروض'
+    Partners.innerHTML = 'شركاءنا'
+    Contact.innerHTML = 'التواصل'
+    Visit.innerHTML = 'قم بزيارتنا'
+    locat.innerHTML = 'الخليج التجاري _ دبي _الإمارات العربية المتحدة '
+    Available.innerHTML = 'متاحين : ٢٤/٧ '
+    Relax.innerHTML = 'استرخي , أنت تحجز لدى برودستي'
+    phone.innerHTML = '+٩٧١٥٠٧٠٠٤٣٤٨'
+    aboutus.innerHTML = 'من نحن ؟'
+    aboutText.innerHTML = " إذا كنت تبحث عن شقة أو عقار للإجار أو البيع فنحن هنا من أجلك"
+    forrent.innerHTML = 'للإيجار'
+    forSale.innerHTML = 'للبيع'
+    ContactUs.innerHTML = 'تواصل معنا'
+    ContactText.innerHTML = 'لدينا فريق خبير ومتكامل من أجلك'
+  }
+
+
+
+
+
+
+
+  else if (getlangauge === "english") {
+    for (let i = 0; i < topFont.length; i++) {
+      topFont[i].style.fontFamily = 'Forum, cursive'
+      
+    }
+    
+    title.innerHTML = " Find a place that touches your soul"
+    for (let i = 0; i < navitem.length; i++) {
+      navitem[i].style.display = "block";
+      navitem[i].style.justifyContent = "none";
+    }
+    home.innerHTML = "Home"
+
+    needusDiv.innerHTML = ` <div class="radio-input">
+  <p style="margin: auto; margin-left: 0;" id="need">Need us for :</p>
+  <input value="value-1" name="value-radio" id="value-1" type="radio">
+  <label for="value-1"><a id="val1A"
+      href="https://api.whatsapp.com/send/?phone=%2B971507004348&text&type=phone_number&app_absent=0">Rent</a></label>
+  <input value="value-2" name="value-radio" id="value-2" type="radio">
+  <label for="value-1"><a id="val2A"
+      href="https://api.whatsapp.com/send/?phone=%2B971507004348&text&type=phone_number&app_absent=0">Sale</a></label>
+  <input value="value-3" name="value-radio" id="value-3" type="radio">
+  <label for="value-1"><a id="val3A"
+      href="https://api.whatsapp.com/send/?phone=%2B971507004348&text&type=phone_number&app_absent=0">Management</a></label>
+</div>`
+
+    About.innerHTML = 'About Us'
+    Latest.innerHTML = 'Latest Offers'
+    Partners.innerHTML = 'Our Partners'
+    Contact.innerHTML = 'Contact'
+    Visit.innerHTML = 'Visit U'
+    locat.innerHTML = ' Business Bay, Dubai, UAE'
+    Available.innerHTML = 'Available: 24/7'
+    Relax.innerHTML = 'Relax, you are booking a Bradosti'
+    phone.innerHTML = ' + 971 50 700 4348'
+    aboutus.innerHTML = 'About Us'
+    aboutText.innerHTML = 'If you are looking for an apartment or property to buy or rent,We are here for you.'
+    forrent.innerHTML = 'For Rent'
+    forSale.innerHTML = 'For Sale'
+    ContactUs.innerHTML = 'Contact Us'
+    ContactText.innerHTML = ' We have an expert team to help you'
+  }
+}
+
